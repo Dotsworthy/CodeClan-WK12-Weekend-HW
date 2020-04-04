53862308f1bdde196b49506d08b520e13346484e
Dotsworthy <dotsworthy@gmail.com>
2020-04-04 12:34:35 +0100

    completed keyboard tests

--- src/main/java/Keyboard.java
+++ src/main/java/Keyboard.java
@@ -1,2 +1,22 @@
-public class Keyboard {
+import EnumerableLists.ColourType;
+import EnumerableLists.InstrumentType;
+import EnumerableLists.MaterialType;
+
+public class Keyboard extends Instrument implements IPlay {
+
+    private boolean hasTouchSensitiveKeys;
+
+    public Keyboard(boolean hasTouchSensitiveKeys, MaterialType material, ColourType colour, InstrumentType instrument) {
+        super(material, colour, instrument);
+        this.hasTouchSensitiveKeys = hasTouchSensitiveKeys;
+
+    }
+
+    public boolean getHasTouchSensitiveKeys() {
+        return hasTouchSensitiveKeys;
+    }
+
+    public String play() {
+        return "Playing keyboard. Plink plonk.";
+    }
 }
