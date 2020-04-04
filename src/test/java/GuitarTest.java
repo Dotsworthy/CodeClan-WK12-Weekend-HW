c7ee8524a0dd648fd7e0300963da84cf7b52c4f8
Dotsworthy <dotsworthy@gmail.com>
2020-04-04 12:06:57 +0100

    added guitar tests

--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -15,4 +15,24 @@ public class GuitarTest {
     public void canGetStrings() {
         assertEquals(6, guitar.getStrings());
     }
+
+    @Test
+    public void canGetMaterial() {
+        assertEquals(MaterialType.WOOD, guitar.getMaterial());
+    }
+    
+    @Test
+    public void canGetColour() {
+        assertEquals("Natural wood", guitar.getColour());
+    }
+
+    @Test
+    public void canGetType() {
+        assertEquals("Acoustic", guitar.getType());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("Playing " + guitar.getStrings() + " string guitar. Strum strum.", guitar.play());
+    }
 }
